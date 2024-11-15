15fc2580aab2c63be72a1f8250d3110708c4e884
kulsakshi <160232998+kulsakshi@users.noreply.github.com>
2024-11-15 16:58:19 +0530

    pune

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # This program adds two numbers
 
-num1 = 1.5
-num2 = 6.3
+num1 = 1.6
+num2 = 6.7
 
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('sakshi')
